2d4a67126647bb0306ea0ca96b28130f467cf5ad
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-05-05 17:04:18 -0700

    add react, browserify, reactify

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
   // Place JavaScript code here...
 
 });
+
+var React = require('react')
+
+class Hello extends React.Component {
+  render(){
+    return <div>Hello, {this.props.name}!</div>
+  }
+}
+
+module.exports = Hello
